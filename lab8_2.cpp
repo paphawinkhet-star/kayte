0ab199cf78628831ea043a1c0d146991be80d403
paphawinkhet-star <paphawin.khet@gmail.com>
2025-12-19 10:07:46 +0700

    lab8_2 findGrade

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,7 +3,20 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double score)
+{
+    if (score > 90)
+        return 'A';
+    else if (score > 75)
+        return 'B';
+    else if (score > 60)
+        return 'C';
+    else if (score > 45)
+        return 'D';
+    else
+        return 'F';
+}
+
 
 int main(){
 	//Input the number of students
